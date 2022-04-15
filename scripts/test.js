4643f5de58b2f2baa86f72b14e16d6c9c101f5a0
nopeless <38830903+nopeless@users.noreply.github.com>
2022-04-15 18:53:53 +0900

    format

--- scripts/test.js
+++ scripts/test.js
@@ -1,25 +1,27 @@
-
-import { FixedTimeoutFIFOMappedQueue } from '../src/fifo.js';
+import { FixedTimeoutFIFOMappedQueue } from "../src/fifo.js";
 
 const fq = new FixedTimeoutFIFOMappedQueue(50, (arg) => {
-  console.log('deleted ' + arg);
+  console.log(`deleted ` + arg);
 });
 
-console.log([...fq.entries()].map(e => e.key).join('->'))
+console.log([...fq.entries()].map((e) => e.key).join(`->`));
 
+// eslint-disable-next-line no-constant-condition
 while (true) {
-  await new Promise(r => setTimeout(r, 10));
+  await new Promise((r) => {
+    setTimeout(r, 10);
+  });
   const r = Math.random();
   const e = Math.floor(Math.random() * 10);
 
   if (r < 0.6) {
-    console.log("appending " + e)
+    console.log(`appending ` + e);
     fq.append(e);
   } else if (r) {
-    console.log("deleting " + e)
-    fq.delete(e)
+    console.log(`deleting ` + e);
+    fq.delete(e);
   }
   console.log(fq.head && fq.head.toString(), fq.tail && fq.tail.toString());
-  console.log([...fq.entryMap.values()].map(e => e.toString()).join(' '));
-  console.log([...fq.entries()].map(e => e && e.toString()).join('->'))
+  console.log([...fq.entryMap.values()].map((e) => e.toString()).join(` `));
+  console.log([...fq.entries()].map((e) => e && e.toString()).join(`->`));
 }
--- scripts/test2.js
+++ scripts/test2.js
@@ -1,23 +1,21 @@
-import { FileSystemCache } from '../src/filecache.js';
-
-
+import { FileSystemCache } from "../src/filecache.js";
 
 const fc = new FileSystemCache({
   ttl: 2000,
   skipInit: true,
-})
+});
 
 await fc.initAsync();
 
-fc.setSync('foo', Buffer.from('bar'));
+fc.setSync(`foo`, Buffer.from(`bar`));
 
-let str = fc.getSync('foo').toString()
+let str = fc.getSync(`foo`).toString();
 
 console.log(str);
 
-await new Promise(r => setTimeout(r, 10000));
+await new Promise((r) => setTimeout(r, 10000));
 
 // Should be null now
-str = fc.getSync('foo');
+str = fc.getSync(`foo`);
 
 console.log(str);
